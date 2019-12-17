f54e5d44a1f6b74d12e666b5b98c3dc1d7b838b4
w1367112248 <1367112248@qq.com>
2019-12-17 10:24:46 +0800

    增加了拦截器

---
+++ src/main/java/com/bbs/interceptor/LoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.bbs.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getSession().getAttribute("userName") != null )
+            return true;
+        else {
+            response.sendRedirect("/User/index");
+            return false;
+        }
+    }
+}
