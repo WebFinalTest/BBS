80b99c9c53dd0ba109ef507c952f52ea5543531f
w1367112248 <1367112248@qq.com>
2019-12-18 17:09:00 +0800

    加入了收藏夹，点赞，和收藏的表现层

---
+++ src/main/java/com/bbs/controller/CollectHandler.java
@@ -0,0 +1,9 @@
+package com.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/Collect")
+@Controller
+public class CollectHandler {
+}
